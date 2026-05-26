7a604e27359921b22ad92edde49448bb03f82306
Galadriel3109 <fridagalavizzuett@gmail.com>
2026-05-26 17:25:41 -0600

    Update workspace via Copilot

---
+++ PL/Controllers/OrdenDummyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace PL.Controllers
+{
+    public class OrdenDummyController : Controller
+    {
+      
+
+        
+
+    }
+}
